a1ab771e6d9345069b9b272d2d6044486c00b617
Binfun <525748752@qq,com>
2017-01-27 13:50:46 +0800

    tab fix

--- add_two_numbers.c
+++ add_two_numbers.c
@@ -14,30 +14,30 @@
  *       */
 
 struct ListNode *list_node_new(int x) {
-	struct ListNode *node = (struct ListNode *)malloc(sizeof(struct ListNode));
-    node->next = NULL;
-    node->val = x;
-	return node;
+        struct ListNode *node = (struct ListNode *)malloc(sizeof(struct ListNode));
+        node->next = NULL;
+        node->val = x;
+        return node;
 }
 
 
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-	struct ListNode* list_p;
-	struct ListNode list_head;
-	int temp_dig = 0, l1v, l2v;
+        struct ListNode* list_p;
+        struct ListNode list_head;
+        int temp_dig = 0, l1v, l2v;
 
-	list_p = &list_head;
-	while(l1 != NULL || l2 != NULL || temp_dig != 0){
-		l1v = l1?l1->val:0;
-        l2v = l2?l2->val:0;
-		temp_dig = temp_dig + l1v + l2v;
-        list_p->next = list_node_new(temp_dig%10);
-		temp_dig /= 10;
-		l1 = l1?l1->next:l1;
-		l2 = l2?l2->next:l2;
-		list_p = list_p->next;
+        list_p = &list_head;
+        while(l1 != NULL || l2 != NULL || temp_dig != 0){
+                l1v = l1?l1->val:0;
+                l2v = l2?l2->val:0;
+                temp_dig = temp_dig + l1v + l2v;
+                list_p->next = list_node_new(temp_dig%10);
+                temp_dig /= 10;
+                l1 = l1?l1->next:l1;
+                l2 = l2?l2->next:l2;
+                list_p = list_p->next;
 
-	};
-	return list_head.next;
+        };
+        return list_head.next;
 }
