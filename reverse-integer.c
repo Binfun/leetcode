18d07ae462f31b0e75d4e837f3a0e8dea5cee00b
Binfun <525748752@qq,com>
2017-03-22 00:24:25 +0800

    not use long type

--- reverse-integer.c
+++ reverse-integer.c
@@ -1,22 +1,14 @@
 //https://leetcode.com/problems/reverse-integer/#/solutions
 int reverse(int x) {
-        int p_n = 0, r = 0;
-        unsigned int stad = -1;stad /= 20;
-        if (x > 0)
-                p_n = 1;
-        else
-                x = -x;
-        if (x < 0)
-                return 0;//except the 0x80000000
-        while(x){
-                if ((r) > stad)
+        int r = 0;
+        unsigned int max_u = -1;max_u /= 20;int max = max_u;
+        unsigned int min_u = -1;int min = (min_u/2 + 1);min /= 10;
+        while(x){printf("[%d]",r);
+                if (r > max || r < min)
                         return 0;
                 r *= 10;
                 r += x % 10;
                 x /= 10;
         };
-        if (p_n) 
-                return r;
-        else
-                return -r;
+        return r;
 }
