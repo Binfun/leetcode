d860073f24df73391cd70c2c9abbea2d9b766796
Binfun <525748752@qq,com>
2017-03-21 20:41:07 +0800

    use constant space

--- first-missing-positive.c
+++ first-missing-positive.c
@@ -1,19 +1,18 @@
 //https://leetcode.com/problems/first-missing-positive/
 int firstMissingPositive(int* nums, int numsSize) {
-        int i;
-        int *oknum;
-        oknum = malloc(sizeof(int)*numsSize);
-        memset(oknum, 0, sizeof(int)*numsSize);
-        for(i = 0; i < numsSize; i++){
-                if(nums[i] > 0 && nums[i] <= numsSize ){
-                        oknum[nums[i] - 1] = 1;
+        int i, temp;
+        for (i = 0; i < numsSize; i++){
+                while (nums[i] <= numsSize && nums[i] > 0 && nums[nums[i] - 1] != nums[i]){
+                        temp = nums[i];
+                        nums[i] = nums[nums[i] - 1];
+                        nums[temp - 1] = temp;
                 }
         }
-
-        for(i = 0; i < numsSize; i++){
-                if(oknum[i] == 0){
+        printf("what!");
+        for (i = 0; i < numsSize; i++){
+                if (nums[i] != i + 1)
                         return (i + 1);
-                }
         }
         return (numsSize + 1);
 }
+
